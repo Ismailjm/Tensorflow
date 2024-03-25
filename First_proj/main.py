facabc95ae11707ac391a3ae63dd55f3d6773a14
Ismail EL JAMIY <104994203+Ismailjm@users.noreply.github.com>
2024-03-25 15:23:27 +0000

    edits

--- First_proj/main.py
+++ First_proj/main.py
@@ -1,2 +1,9 @@
 import tensorflow as tf
-print(tf.__version__)
+
+print("TensorFlow version:", tf.__version__)
+
+# Check if GPU support is available
+if tf.test.is_built_with_cuda():
+    print("TensorFlow is installed with GPU support")
+else:
+    print("TensorFlow is installed with CPU support")
